5d41f36d30bb533eebf2ed4e798130f3397a9651
Atsusi <114609407+Atsusi0327@users.noreply.github.com>
2022-10-31 14:20:23 +0800

    First Test

---
+++ ImageSelect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImageSelect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
